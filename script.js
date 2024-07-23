7f6bc79b187c249cf23a61a58a2a1e1007c70b1f
bharathi <bharathiraj7688@gmail.com>
2024-07-23 21:39:48 +0530

    js

--- script.js
+++ script.js
@@ -2,3 +2,16 @@ function myFunction() {
     var element = document.body;
     element.classList.toggle("dark-mode");
  }
+
+ 
+var header = document.getElementById("nav-items");
+var link = header.getElementsByClassName("act");
+for (var i = 0; i < link.length; i++) {
+  link[i].addEventListener("click", function() {
+  var current = document.getElementsByClassName("active");
+  if (current.length > 0) { 
+    current[0].className = current[0].className.replace(" active", "");
+  }
+  this.className += " active";
+  });
+}
