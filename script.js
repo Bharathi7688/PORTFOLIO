67d8051de8228a4c6ed9dd417f1d52641bc0f1d3
bharathi <bharathiraj7688@gmail.com>
2024-07-24 18:02:28 +0530

    nav

--- script.js
+++ script.js
@@ -5,9 +5,9 @@ function myFunction() {
 
  
 var header = document.getElementById("nav-items");
-var link = header.getElementsByClassName("act");
-for (var i = 0; i < link.length; i++) {
-  link[i].addEventListener("click", function() {
+var nav = header.getElementsByClassName("act"); 
+for (var i = 0; i < nav.length; i++) {
+  nav[i].addEventListener("click", function() {
   var current = document.getElementsByClassName("active");
   if (current.length > 0) { 
     current[0].className = current[0].className.replace(" active", "");
